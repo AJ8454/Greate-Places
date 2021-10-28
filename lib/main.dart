68b5e74103e0a3b13727f9309977a3b8b5d1139f
AJ8454 <ajaypangare8454@gmail.com>
2021-10-28 17:31:13 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/provider/great_places.dart';
+import 'package:provider/provider.dart';
+
+import 'screens/places_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'Greate Place',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo)
+              .copyWith(secondary: Colors.amber),
+        ),
+        home: const PlacesListScreen(),
+      ),
+    );
+  }
+}
